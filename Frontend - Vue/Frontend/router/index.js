8d4d5c7dca17294876e91bf20bfdb1ef640956d1
Dusan Tomic <duletomic14901@gmail.com>
2025-02-02 16:44:29 +0100

    Optimizacija u index.js

--- Frontend - Vue/Frontend/router/index.js
+++ Frontend - Vue/Frontend/router/index.js
@@ -11,21 +11,38 @@ import EditProduct from '../views/EditProduct.vue';
 import EditOrder from '../views/EditOrder.vue';
 
 const routes = [
-    { path: '/', component: Home },
-    { path: '/users', component: Users },
-    { path: '/add-user', component: AddUser},
-    { path: '/products', component: Products },
-    { path: '/orders', component: Orders },
-    { path: '/add-product', component: AddProduct },
-    { path: '/add-order', component: AddOrder },
-    { path: '/edituser/:id', component: EditUser },
-    { path: '/editproduct/:id', component: EditProduct },
-    { path: '/editorder/:id', component: EditOrder },
+    { path: '/', component: Home, meta: { requiresAuth: true } },
+    { path: '/users', component: Users, meta: { requiresAuth: true } },
+    { path: '/add-user', component: AddUser, meta: { requiresAuth: true }},
+    { path: '/products', component: Products, meta: { requiresAuth: true } },
+    { path: '/orders', component: Orders, meta: { requiresAuth: true } },
+    { path: '/add-product', component: AddProduct, meta: { requiresAuth: true } },
+    { path: '/add-order', component: AddOrder, meta: { requiresAuth: true } },
+    { path: '/edituser/:id', component: EditUser, meta: { requiresAuth: true } },
+    { path: '/editproduct/:id', component: EditProduct, meta: { requiresAuth: true } },
+    { path: '/editorder/:id', component: EditOrder, meta: { requiresAuth: true } },
 ];
 
 const router = createRouter({
     history: createWebHistory(),
     routes
+});
+
+router.beforeEach((to, from, next) => {
+    const isAuthenticated = !!localStorage.getItem("access_token"); // Provera tokena
+    const userRole = localStorage.getItem("user_role"); // Dobijanje uloge
+
+    if (to.meta.requiresAuth && !isAuthenticated) {
+        if (to.path !== "/") {  // ❗ Sprečava beskonačno preusmeravanje
+            next('/'); 
+        } else {
+            next(); // Ako je već na login stranici, dozvoli prikaz
+        }
+    } else if (to.meta.requiresAdmin && userRole !== 'admin') {
+        next('/'); 
+    } else {
+        next(); // Dozvoljavamo pristup
+    }
 });
 
 export default router;
